501c80d6222843c26050afa58fee66a26e00f0c9
tebarek94 <tebarekabas4@gmail.com>
2025-11-15 04:40:26 +0300

    first commit

---
+++ packages/server/index.ts
@@ -0,0 +1,17 @@
+import express, { type Request, type Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from the server!");
+});
+app.get("/api-key", (req: Request, res: Response) => {
+  res.send({ apiKey: process.env.OPENAI_API_KEY || "No API Key Found" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
